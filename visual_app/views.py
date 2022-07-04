f8d45c5d915600efa12419dfc26c7ff0ca0d9bef
JessicaAGF <jessica.gajardo@ug.uchile.cl>
2022-07-04 15:01:41 -0400

    gráfico 4 y 5 working

--- visual_app/views.py
+++ visual_app/views.py
@@ -1,18 +1,20 @@
-from django.db.models.functions import TruncMonth, ExtractMonth, ExtractYear, ExtractHour
+from django.db.models.functions import TruncMonth, ExtractMonth, ExtractYear, ExtractHour, ExtractDay
 from django.shortcuts import render
 from .models import *
-from django.db.models import Count, Sum, Max
+from django.db.models import Count, Sum, Max, F
 
 
 # Create your views here.
 def index(request):
-    product_qtt = Product.objects.values('name').order_by('name').annotate(qtt=Sum('quantity'))
-    # products_name1 = Product.objects.values('name').order_by('name').distinct()
-    # product_cat_qtt = Product.objects.values('name', 'category').order_by('category').annotate(qtt=Sum('quantity'))
-    # product_qtt = Product.objects.raw('SELECT name, SUM(quantity) AS qtt FROM visual_app_Product GROUP BY name')
+    # frecuencia de productos
+    product_qtt = Product.objects.values('name').annotate(qtt=Sum('quantity')).order_by('name')
+    # ingresos por producto
+    product_income = Product.objects.values('name').annotate(qtt=Sum('quantity')*F('price')).order_by('name')
+    # ingreso mensual
     income_month = Sale.objects.annotate(month=ExtractMonth('opened'), year=ExtractYear('opened')).values(
         'month', 'year').annotate(income=Sum('total'))
 
+    # clientes por hora
     a_client_per_hour = []
     for i in range(25):
         client_per_hour = Sale.objects.annotate(inithour=ExtractHour('opened'), finhour=ExtractHour('closed')).filter(
@@ -20,6 +22,11 @@ def index(request):
         if client_per_hour['diners__sum'] is not None:
             client_per_hour['hour'] = i
             a_client_per_hour += [client_per_hour]
+
+    # ingreso promedio
+    total_days = len(Sale.objects.annotate(day=ExtractDay('opened'), year=ExtractYear('opened'),
+                                       month=ExtractMonth('opened')).values('day','month','year').distinct())
+    income_per_pm = Payment.objects.values('type').annotate(income=Sum('amount')/total_days)
 
 
     context = {
@@ -31,5 +38,7 @@ def index(request):
         'len2': len(income_month),
         'clientsph': a_client_per_hour,
         'len3': len(a_client_per_hour),
+        'avg_income_ppm': income_per_pm,
+        'product_income': product_income
     }
     return render(request, 'main/index.html', context)
