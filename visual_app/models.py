dac4cda944e376fecf3cb9403f087beb29346372
JessicaAGF <jessica.gajardo@ug.uchile.cl>
2022-07-04 03:05:48 -0400

    gráfico 1, 2 y 3 working

--- visual_app/models.py
+++ visual_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Product(models.Model):
     sale_id = models.CharField(max_length=200)
@@ -34,4 +35,3 @@ class Sale(models.Model):
 
     def __str__(self):
         return self.sale_id
-
--- visual_app/views.py
+++ visual_app/views.py
@@ -1,18 +1,35 @@
+from django.db.models.functions import TruncMonth, ExtractMonth, ExtractYear, ExtractHour
 from django.shortcuts import render
 from .models import *
-from django.db.models import Count, Sum
+from django.db.models import Count, Sum, Max
 
 
 # Create your views here.
 def index(request):
-
-    #products_name1 = Product.objects.values('name').order_by('name').distinct()
     product_qtt = Product.objects.values('name').order_by('name').annotate(qtt=Sum('quantity'))
-    #product_qtt = Product.objects.raw('SELECT name, SUM(quantity) AS qtt FROM visual_app_Product GROUP BY name')
+    # products_name1 = Product.objects.values('name').order_by('name').distinct()
+    # product_cat_qtt = Product.objects.values('name', 'category').order_by('category').annotate(qtt=Sum('quantity'))
+    # product_qtt = Product.objects.raw('SELECT name, SUM(quantity) AS qtt FROM visual_app_Product GROUP BY name')
+    income_month = Sale.objects.annotate(month=ExtractMonth('opened'), year=ExtractYear('opened')).values(
+        'month', 'year').annotate(income=Sum('total'))
+
+    a_client_per_hour = []
+    for i in range(25):
+        client_per_hour = Sale.objects.annotate(inithour=ExtractHour('opened'), finhour=ExtractHour('closed')).filter(
+            inithour__lte=i, finhour__gte=i).aggregate(Sum('diners'))
+        if client_per_hour['diners__sum'] is not None:
+            client_per_hour['hour'] = i
+            a_client_per_hour += [client_per_hour]
+
 
     context = {
         'product_name': product_qtt.values('name'),
         'product_qtt': product_qtt.values('qtt'),
         'len1': len(product_qtt),
+        'income_month': income_month.values('month', 'year'),
+        'income': income_month.values('income'),
+        'len2': len(income_month),
+        'clientsph': a_client_per_hour,
+        'len3': len(a_client_per_hour),
     }
-    return render(request, 'main/index.html',context)
+    return render(request, 'main/index.html', context)
